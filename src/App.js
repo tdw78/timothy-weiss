be4ef6a9022633c26ecfe646cf65138d275eb003
Tim Weiss <tdweiss1978@gmail.com>
2020-06-03 19:51:44 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <span class="text-uppercase">Timothy Weiss</span>
+        </header>
+        
+        <main>
+          <p>Welcome to My Site</p>
+        </main>
+
+      </div>
+    );
+  }
+}
+
+export default App;
